547779744075718d012a5faaba0b3a33bec29ef3
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-07-23 20:36:55 +0200

    set build and push on ECR

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from stack import AppStack
 
 app = App()
 
-AppStack(app, "AppStack", repository_name="webapp", stage="staging", image_tag="0.0.1", push_image=True)
+AppStack(app, "Ec2ServiceStack", repository_name="ec2-service", stage="staging", image_tag="0.0.1", push_image=True)
 
 app.synth()
--- stack.py
+++ stack.py
@@ -1,4 +1,10 @@
-from aws_cdk import CfnOutput, Stack, Stage
+from aws_cdk import CfnOutput, Stack, Stage, RemovalPolicy
+from aws_cdk import (
+    aws_ecr as ecr,
+    aws_codebuild as codebuild,
+    aws_s3
+
+)
 from constructs import Construct
 
 
@@ -6,6 +12,10 @@ class AppStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, repository_name: str, stage: Stage, image_tag: str, push_image: bool, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        
+        docker_repository = ecr.Repository(self, f"{repository_name}-{stage}-docker_repository",
+            repository_name=repository_name,
+            removal_policy=RemovalPolicy.DESTROY
+        )
 
         CfnOutput(self, "StackRegion", value=self.region, description="AWS Region")
+        CfnOutput(self, "DockerRepositoryUri", value=docker_repository.repository_uri, description="Docker Repository Url")
