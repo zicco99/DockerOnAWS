095f113b049ef09c38257b07dbf646daa1229a48
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-07-24 12:35:44 +0200

    path

--- stack.py
+++ stack.py
@@ -39,7 +39,7 @@ class AppStack(Stack):
             ),
             source=codebuild.Source.s3(
                 bucket=source_bucket,
-                path="microservice"
+                path="microservice/"
             ),
             build_spec=codebuild.BuildSpec.from_object({
                 'version': '0.2',
