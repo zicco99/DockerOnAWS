ece4aadf0d7af68baca44b5b3d6c6a33d8cc1652
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-06 16:01:33 +0200

    using fargate to run docker image

--- stack.py
+++ stack.py
@@ -6,6 +6,9 @@ from aws_cdk import (
     aws_codebuild as codebuild,
     aws_iam as iam,
     aws_s3 as s3,
+    aws_ec2 as ec2,
+    aws_ecs as ecs,
+    aws_logs as logs,
     aws_s3_deployment as s3_deployment,
     aws_events as events,
     aws_events_targets as targets,
@@ -13,48 +16,60 @@ from aws_cdk import (
 from constructs import Construct
 
 class AppStack(Stack):
+    # From constructor extract image tag, stage and repository name
     def __init__(self, scope: Construct, construct_id: str, repository_name: str, stage: str, image_tag: str, push_image: bool, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        source_bucket = s3.Bucket(self, f"{repository_name}-{stage}-source_bucket",
-            bucket_name=f"{repository_name}-{stage}-source-bucket",
-            removal_policy=RemovalPolicy.DESTROY,
-            auto_delete_objects=True
-        )
+        #-----------------
+        #   Setup  -
+        #-----------------
 
-        s3_deployment.BucketDeployment(self, f"{repository_name}-{stage}-s3_deployment",
-            sources=[s3_deployment.Source.asset("./microservice")],
-            destination_bucket=source_bucket,
-            destination_key_prefix="microservice"
-        )
-        
-        docker_repository = ecr.Repository(self, f"{repository_name}-{stage}-docker_repository",
+        #VPC with 2 AZs
+        main_vpc = ec2.Vpc(self, "{}", max_azs=2)
+
+        # Define ECR Repository
+        docker_repository = ecr.Repository(self, f"{repository_name}-{stage}-repository",
             repository_name=repository_name,
             removal_policy=RemovalPolicy.DESTROY
         )
 
+        self.ecr_repository = docker_repository
+
+        
+
+        #-----------------
+        #   Dockerizing  -
+        #-----------------
+        # Dockerizing the microservice
+
+        image_tag = codebuild.BuildEnvironmentVariable(value=image_tag) + '-' + stage + '-latest'
+
+        source_bucket = s3.Bucket(self, f"{repository_name}-{stage}-source_bucket",
+            bucket_name=f"{repository_name}-{stage}-source-bucket",
+            removal_policy=RemovalPolicy.DESTROY, 
+            auto_delete_objects=True 
+        )
+
+        docker_repository = ecr.Repository(self, f"{repository_name}-{stage}-docker_repository",
+            repository_name=repository_name,
+            removal_policy=RemovalPolicy.DESTROY  # Ensures repository is deleted when the stack is deleted
+        )
+
+        # CodeBuild project to build Docker images
         build_project = codebuild.Project(self, f"{repository_name}-{stage}-microservice_build_project",
             environment=codebuild.BuildEnvironment(
-                build_image=codebuild.LinuxBuildImage.STANDARD_7_0,
-                privileged=True,
+                build_image=codebuild.LinuxBuildImage.STANDARD_7_0,  # Use standard Linux build image
+                privileged=True,  # Required to build Docker images
                 environment_variables={
-                    'AWS_ACCOUNT_ID': codebuild.BuildEnvironmentVariable(
-                        value=self.account
-                    ),
-                    'AWS_DEFAULT_REGION': codebuild.BuildEnvironmentVariable(
-                        value=self.region
-                    ),
-                    'REPOSITORY_URI': codebuild.BuildEnvironmentVariable(
-                        value=docker_repository.repository_uri
-                    ),
-                    'IMAGE_TAG': codebuild.BuildEnvironmentVariable(
-                        value=image_tag
-                    )
+                    'AWS_ACCOUNT_ID': codebuild.BuildEnvironmentVariable(value=self.account),
+                    'AWS_DEFAULT_REGION': codebuild.BuildEnvironmentVariable(value=self.region),
+                    'REPOSITORY_URI': codebuild.BuildEnvironmentVariable(value=docker_repository.repository_uri),
+                    'IMAGE_TAG': image_tag
                 }
             ),
             source=codebuild.Source.s3(
                 bucket=source_bucket,
-                path="microservice/"
+                path="microservice/"  # Path within the S3 bucket where the source code is stored
             ),
             build_spec=codebuild.BuildSpec.from_object({
                 'version': '0.2',
@@ -63,8 +78,7 @@ class AppStack(Stack):
                         'commands': [
                             'echo Logging in to Amazon ECR...',
                             'aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $REPOSITORY_URI',
-                            'COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)',
-                            'IMAGE_TAG=${COMMIT_HASH:=latest}'
+                            'COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)'
                         ]
                     },
                     'build': {
@@ -85,30 +99,83 @@ class AppStack(Stack):
             })
         )
 
-        docker_repository.grant_pull_push(build_project.role)
         
+
+        #-----------------
+        #   Permissions -
+        #-----------------
+
+        docker_repository.grant_pull_push(build_project.role)
+
         build_project.add_to_role_policy(iam.PolicyStatement(
             actions=["s3:GetObject"],
             resources=[f"{source_bucket.bucket_arn}/*"]
         ))
-        
+
         build_project.add_to_role_policy(iam.PolicyStatement(
             actions=["ecr:GetAuthorizationToken"],
             resources=["*"]
         ))
 
+        s3_deployment.BucketDeployment(self, f"{repository_name}-{stage}-s3_deployment",
+            sources=[s3_deployment.Source.asset("./microservice")],
+            destination_bucket=source_bucket,
+            destination_key_prefix="microservice"  # Folder within the S3 bucket to upload to
+        )
+
         rule = events.Rule(self, f"{repository_name}-{stage}-rule",
             event_pattern=events.EventPattern(
                 source=["aws.s3"],
                 detail_type=["AWS API Call via CloudTrail"],
-                resources=[source_bucket.bucket_arn],
                 detail={
-                    'eventName': ['PutObject', 'CopyObject', 'CompleteMultipartUpload']
+                    'eventName': ['PutObject', 'CopyObject', 'CompleteMultipartUpload'],
+                    'requestParameters': {
+                        'bucketName': [source_bucket.bucket_name]
+                    }
                 }
             )
         )
 
         rule.add_target(targets.CodeBuildProject(build_project))
 
+
+        #-----------------
+        #   Deployment  -
+        #-----------------
+
+        # Define all is needed for a fargate service
+
+        cluster = ecs.Cluster(self, f"{repository_name}-{stage}-cluster",
+            cluster_name=f"{repository_name}-{stage}-cluster",
+            vpc=main_vpc
+        )
+
+        log_group = logs.LogGroup(self, f"{repository_name}-{stage}-log-group",
+            log_group_name=f"/ecs/{repository_name}-{stage}-log-group",
+            removal_policy=RemovalPolicy.DESTROY
+        )
+
+        task_definition = ecs.FargateTaskDefinition(self,f"{repository_name}-{stage}-task-definition",
+            memory_limit_mib=512,
+            cpu=256,
+        )
+
+        task_definition.add_container("MicroServiceContainer",
+            image=ecs.ContainerImage.from_ecr_repository(docker_repository, tag=image_tag),
+            logging=ecs.LogDrivers.aws_logs(
+                log_group=log_group,
+                stream_prefix="ecs"
+            ),
+            port_mappings=[ecs.PortMapping(container_port=80)]
+        )
+
+        service = ecs.FargateService(self, f"{repository_name}-{stage}-service",
+            cluster=cluster,
+            task_definition=task_definition,
+            desired_count=1,
+            assign_public_ip=True
+        )
+
+        CfnOutput(self, "ServiceUrl", value=service.load_balancer_target.service.load_balancer.load_balancer_dns_name, description="Service URL")
         CfnOutput(self, "StackRegion", value=self.region, description="AWS Region")
-        CfnOutput(self, "DockerRepositoryUri", value=docker_repository.repository_uri, description="Docker Repository Url")
+        CfnOutput(self, "DockerRepositoryUri", value=docker_repository.repository_uri, description="Docker Repository URL")
