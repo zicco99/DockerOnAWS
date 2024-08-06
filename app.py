c1e5e0f1bb264e9b5a0a618a1c4a8e4af5ee1045
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-06 18:02:21 +0200

    changing repo name to grant uniqueness in s3

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from stack import AppStack
 
 app = App()
 
-AppStack(app, "Ec2ServiceStack", repository_name="ec2-service", stage="staging", image_tag="0.0.1", push_image=True)
+AppStack(app, "Ec2ServiceStack", repository_name="zik-fargate-service", stage="staging", image_tag="0.0.1", push_image=True)
 
 app.synth()
